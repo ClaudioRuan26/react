d571e2ef2b420f13f8c0e116d827003c97d7592c
ClaudioRuan26 <claudioruan25@gmail.com>
2025-08-26 12:45:13 -0300

    Primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import AddTask from "./components/AddTask"
+import Tasks from "./components/Tasks"
+import {v4} from 'uuid';
+
+function App() {
+  const [tasks, setTasks] = useState([{
+      id: 1,
+      title: "Estudar prog",
+      description: "Estudar react",
+      completed: false
+    },
+    {
+      id: 2,
+      title: "Estudar react",
+      description: "Estudar hooks",
+      completed: false
+    }
+  ]);
+
+  function onTaskClick(taskId) {
+
+    const newTasks = tasks.map(task =>
+      {if (task.id == taskId) {
+        return {...task, isCompleted: !task.isCompleted}
+      }
+
+      return task;
+
+    });
+
+    setTasks(newTasks);
+
+  }
+
+  function onDeleteTaskClick(taskId) {
+
+    const newTasks = tasks.filter(task => task.id !== taskId);
+    setTasks(newTasks);
+
+  }
+
+  function onAddTaskClick(title, description) {
+
+    const newTasks = {
+        id: v4(),
+        title: title,
+        description: description,
+        completed: false
+      };
+    setTasks([...tasks, newTasks]);
+
+  }
+
+  return (
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p6">
+      <div className="w-[500px] space-y-4">
+        <h1 className="text-3xl text-slate-100 font-bold text-center my-2">Gerenciador de Tarefas</h1>
+        <AddTask 
+          onAddTaskClick={onAddTaskClick}
+        />
+        <Tasks 
+          tasks={tasks} 
+          onTaskClick={onTaskClick} 
+          onDeleteTaskClick={onDeleteTaskClick} 
+        />
+      </div>
+    </div>
+  )
+}
+
+export default App
